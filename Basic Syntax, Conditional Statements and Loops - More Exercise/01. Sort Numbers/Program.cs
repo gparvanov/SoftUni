76fe54e70bf6b72a91b3688aa5d7cde3ffde20fa
Georgi Parvanov <56466239+gparvanov@users.noreply.github.com>
2020-02-05 23:37:53 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDigit = int.Parse(Console.ReadLine());
+            int secondDigit = int.Parse(Console.ReadLine());
+            int thirdDigit = int.Parse(Console.ReadLine());
+
+            int maxDigit = (Math.Max(Math.Max(firstDigit, secondDigit), thirdDigit));
+            int minDigit = (Math.Min(Math.Min(firstDigit, secondDigit), thirdDigit));
+            Console.WriteLine(maxDigit);
+            if (firstDigit > minDigit && firstDigit < maxDigit) Console.WriteLine(firstDigit);
+            else if (secondDigit > minDigit && secondDigit < maxDigit) Console.WriteLine(secondDigit);
+            else Console.WriteLine(thirdDigit);           
+            Console.WriteLine(minDigit);
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/03. Gaming Store/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _03._Gaming_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float budget = float.Parse(Console.ReadLine());
+
+            float spentMoney = 0;
+
+            string command = "";
+
+            while(command!="Game Time")
+            {
+                command = Console.ReadLine();
+                float gamePrice = 0;
+                if (command == "Game Time") break;
+                else if (command == "OutFall 4") gamePrice = 39.99F;
+                else if (command == "CS: OG") gamePrice = 15.99F;
+                else if (command == "Zplinter Zell") gamePrice = 19.99F;
+                else if (command == "Honored 2") gamePrice = 59.99F;
+                else if (command == "RoverWatch") gamePrice = 29.99F;
+                else if (command == "RoverWatch Origins Edition") gamePrice = 39.99F;
+                else
+                {
+                    Console.WriteLine("Not Found");
+                    continue;
+                }
+
+                if (budget >= gamePrice)
+                {
+                    budget -= gamePrice;
+                    spentMoney += gamePrice;
+                    Console.WriteLine($"Bought {command}");
+                }
+                else
+                {
+                    Console.WriteLine("Too Expensive");
+                    continue;
+                }
+                if (budget == 0)
+                {
+                    Console.WriteLine("Out of money!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Total spent: ${spentMoney:F2}. Remaining: ${budget:F2}");
+        }
+    }
+}
