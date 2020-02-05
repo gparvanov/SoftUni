9e4d628f42cae7484dba6c57060672bc16add8e5
Georgi Parvanov <56466239+gparvanov@users.noreply.github.com>
2020-02-05 23:33:47 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise1/10. Rage Expenses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            float headSetPrice = float.Parse(Console.ReadLine());
+            float mousePrice = float.Parse(Console.ReadLine());
+            float keyboardPrice = float.Parse(Console.ReadLine());
+            float displayPrice = float.Parse(Console.ReadLine());
+
+            int crashedHeadset = lostGamesCount / 2; 
+            int crashedMouse = lostGamesCount / 3;
+            int crashedKeyboard = lostGamesCount / 6;
+            int crashedDisplay = lostGamesCount / 12;
+
+
+            float headsetExpences = crashedHeadset * headSetPrice;
+            float mouseExpences = crashedMouse * mousePrice;
+            float keyboardExpences = crashedKeyboard * keyboardPrice;
+            float displayExpences = crashedDisplay * displayPrice;
+
+            float totalExpences = headsetExpences + mouseExpences + keyboardExpences + displayExpences;
+
+            Console.WriteLine($"Rage expenses: {totalExpences:F2} lv.");
+
+        }
+    }
+}
